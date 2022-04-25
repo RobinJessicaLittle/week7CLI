b4a77ea9c42075a95a43ea5034bcd105af1d12b3
RobinJessicaLittle <robin.jessica.little@outlook.com>
2022-04-25 16:36:32 +0100

    checking my code

--- src/app.js
+++ src/app.js
@@ -1,24 +1,61 @@
 
-//
-const yargs = require('yargs');
+// save yargs to variable using require keyword
+const yargs = require("yargs");
 
-const {Movie, movieArr} = require('./utils')
+// require objects we need from utils folder
+// by default node looks for index.js, so we don't need to specify
+const { Movie, movieArr } = require("./utils")
 
-// console.log(process.argv);
+// example of yargs structure
 console.log(yargs.argv);
 
-             //placeholder
+// functinality to check user input and save data
+// pass in YargsObj as permameter 
 const app = (yargsObj) => {
     if(yargsObj.add) {
-        const movie = new Movie(yargsObj.title, yargsObj.actor);
+        // use new Movie class to collect data
+        const movie = new Movie(yargsObj.title, yargsObj.actor, yargsObj.genre);
+        // use add function to save data to array
         movie.add();
         console.log(movieArr);
-    } else {
-        console.log('incorrect Commande')
+        //added as part of task
+    }else if(yargsObj.movie) { 
+        const movie = new Movie(yargsObj.title, yargsObj.actor, yargsObj.genre);
+        movie.mov();
+        console.log(movieArr)
+
+
+    }else {
+        console.log("Incorrect Command")
     }
 };
 
+// call app function and pass in terminal input as arguements
 app(yargs.argv);
 
-//node src/app.js --add --title='pulp fiction' --actor='Sam L Jackson'
+//node src/app.js --add --title='pulp fiction' --actor='Sam L Jackson' too print if
+//node src/app.js --mov --title='green mile' --actor='Tom Hanks' --genre='thriller' too print else if
 //put above in terminal to return object and what you want to add is inside the quotes
+
+
+////////////////////////////////////////////////
+//My attempt at yargs, cat app
+// const yargs = require('yargs');
+
+// const {Cats, catsArr} = require('./utils')
+
+// console.log(yargs.argv);
+
+// const app2 = (yargsObj) => {
+//     if (yargsObj.add) {
+//         const plants = new Cats (yargsObj.breed, yargsObj.color, yargsObj.age);
+//         plants.cat();
+//     }else{
+//         console.log('Cat not recognised')
+//     }
+// };
+// app2(yargs.argv)
+
+// src/app.js --add --breed='moggy' --color='grey' --age='9'
+//when example is commented out (as yargs has already been declared)
+//my rty works in the terminal
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,46 @@
+// npm innit -y, initialises the environment
+
+// npm install yargs
+
+
+// touch .gitignore
+
+
 const movieArr = [];
 // create Movie class for title and actor object
 //this keyword refers to the object it belongs to
 class Movie {
-    constructor(title, actor = 'Not specified'){
+    constructor(title, actor, genre = 'Not specified'){
         this.title = title;
         this.actor = actor;
+        this.genre = genre;
     }
     // small function to add cli input to movieArr
     add() {
         movieArr.push(this)
     }
+    //added as part of task
+    mov() {
+        movieArr.push(this)
+    }
 };
 //export objects to use elsewhere in app
 module.exports = {Movie, movieArr};
+
+////////////////////////////////////////////////
+//My attempt at yargs, cat app
+// const catsArr = [];
+
+// class Cats {
+//     constructor(breed, color, age = 'Not specified'){
+//         this.breed = breed;
+//         this.color = color;
+//         this.age = age;
+//     }
+//     cat() {
+//         catsArr.push(this)
+//     }
+// };
+
+// module.exports = {Cats, catsArr};
+
